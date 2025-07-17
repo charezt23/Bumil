5b0ca52ee8871a7854ab135b3139a004f86568ff
charezt23 <kharis.subani@gmail.com>
2025-07-18 00:13:57 +0700

    feat: add logic to reject duplicate entry on imunisasi table by balita

--- app/Http/Controllers/ImunisasiController.php
+++ app/Http/Controllers/ImunisasiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Imunisasi;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 
 class ImunisasiController extends Controller
@@ -14,9 +15,18 @@ class ImunisasiController extends Controller
         try {
             $validated = $request->validate([
                 'balita_id' => 'required|integer|exists:balita,id',
-                'jenis_imunisasi' => 'required|string|max:255',
+                'jenis_imunisasi' => [
+                    'required','string','max:255',
+                    Rule::unique('imunisasi')->where(function ($query) use ($request) {
+                        return $query->where('balita_id', $request->balita_id)
+                            ->where('jenis_imunisasi', $request->jenis_imunisasi);
+                    }),
+                ],
                 'tanggal_imunisasi' => 'required|date'
+            ], [
+                'jenis_imunisasi.unique' => 'Jenis Imunisasi : ' . $request->jenis_imunisasi . ' sudah diisi.',
             ]);
+
 
             $imunisasi = Imunisasi::create($validated);
             $imunisasi->load('balita');
@@ -77,9 +87,17 @@ class ImunisasiController extends Controller
             $imunisasi = Imunisasi::findOrFail($id);
 
             $validated = $request->validate([
-                'balita_id' => 'sometimes|required|integer|exists:balita,id',
-                'jenis_imunisasi' => 'sometimes|required|string|max:255',
-                'tanggal_imunisasi' => 'sometimes|required|date'
+                'balita_id' => 'sometimes|integer|exists:balita,id',
+                'jenis_imunisasi' => [
+                    'sometimes','string','max:255',
+                    Rule::unique('imunisasi')->where(function ($query) use ($request, $imunisasi) {
+                        return $query->where('balita_id', $request->balita_id?? $imunisasi->balita_id)
+                            ->where('jenis_imunisasi', $request->jenis_imunisasi);
+                    })->ignore($imunisasi->id),
+                ],
+                'tanggal_imunisasi' => 'sometimes|date'
+            ], [
+                'jenis_imunisasi.unique' => 'Jenis Imunisasi : ' . $request->jenis_imunisasi . ' sudah ada silahkan hapus '. $request->jenis_imunisasi . ' terlebih dahulu untuk merubah ini.',
             ]);
 
             $imunisasi->update($validated);
@@ -137,28 +155,28 @@ class ImunisasiController extends Controller
         }
     }
     public function GetImunisasibyBalita($balitaId): JsonResponse
-        {
-            try {
-                $imunisasi = Imunisasi::where('balita_id', $balitaId)->get();
+    {
+        try {
+            $imunisasi = Imunisasi::where('balita_id', $balitaId)->get();
 
-                if ($imunisasi->isEmpty()) {
-                    return response()->json([
-                        'success' => false,
-                        'message' => 'Data imunisasi tidak ditemukan untuk balita ini'
-                    ], 404);
-                }
-
-                return response()->json([
-                    'success' => true,
-                    'message' => 'Data imunisasi berhasil diambil',
-                    'data' => $imunisasi
-                ], 200);
-            } catch (\Exception $e) {
+            if ($imunisasi->isEmpty()) {
                 return response()->json([
                     'success' => false,
-                    'message' => 'Gagal mengambil data imunisasi',
-                    'error' => $e->getMessage()
-                ], 500);
+                    'message' => 'Data imunisasi tidak ditemukan untuk balita ini'
+                ], 404);
             }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Data imunisasi berhasil diambil',
+                'data' => $imunisasi
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Gagal mengambil data imunisasi',
+                'error' => $e->getMessage()
+            ], 500);
         }
+    }
 }
